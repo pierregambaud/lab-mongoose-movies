88a500c8ae48b3edd88459040f038b9cbaea8dca
blablamedia <pierre@gambaud.com>
2019-12-09 14:38:16 +0100

    iteration 8: done

--- starter-code/app.js
+++ starter-code/app.js
@@ -51,11 +51,13 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
+const celebritiesRouter = require('./routes/celebrities.js');
+app.use('/celebrities', celebritiesRouter);
 
-const celebsRouter = require('./routes/celebrities.js');
-app.use('/celebrities', celebsRouter);
+const moviesRouter = require('./routes/movies.js');
+app.use('/movies', moviesRouter);
 
 module.exports = app;
---
+++ starter-code/routes/movies.js
@@ -0,0 +1,31 @@
+const express = require(`express`);
+const router = express.Router();
+const Movie = require(`../models/movie`);
+const Celebrity = require(`../models/celebrity`);
+
+router.get(`/new`, function(req,res,next) {
+  Celebrity.find()
+  .then(function(celebrities) {
+    res.render(`movies/new`, {
+      celebrities: celebrities
+    })
+  })
+  .catch(err => next(err))
+})
+
+router.post(`/`, function(req,res,next) {
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.create({
+    title,
+    genre,
+    plot,
+    cast
+  })
+  .then(
+    res.redirect(`/movies`)
+  )
+  .catch(err => next(err))
+})
+
+module.exports = router;
